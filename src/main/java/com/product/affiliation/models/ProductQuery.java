838307dd07a46dcd2fbe9546454290aa6273679c
Pankaj Pardasani <pankaj.d.p@gmail.com>
2024-09-25 22:58:30 +0100

    query pojo and its jackson annotations

---
+++ src/main/java/com/product/affiliation/models/ProductQuery.java
@@ -0,0 +1,40 @@
+package com.product.affiliation.models;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+public class ProductQuery {
+  @JsonSetter("k")
+  private String key;
+  @JsonSetter("v")
+  private Object value;
+  @JsonSetter("operator")
+  private Operator operation;
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public Object getValue() {
+    return value;
+  }
+
+  public void setValue(Object value) {
+    this.value = value;
+  }
+
+  public Operator getOperation() {
+    return operation;
+  }
+
+  public void setOperation(Operator operation) {
+    this.operation = operation;
+  }
+
+  public enum Operator {
+    GT, LT, IS
+  }
+}
